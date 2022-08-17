51a36019269318cd5451714a74488ba2e45213fa
charlotte-greatbatch <charlottegreatbatch01@gmail.com>
2022-08-18 11:05:01 +1200

    added small things for the note.js

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,5 +1,15 @@
+import { MdDeleteForever } from 'react-icons/md'
+
 const Note = () => {
-  return <div className="note"></div>
+  return (
+    <div className="note">
+      <span>Hello this is our first note</span>
+      <div className="note-footer">
+        <small>/8/18/2022</small>
+        <MdDeleteForever className="delete-icon" size="1.3em" />
+      </div>
+    </div>
+  )
 }
 
 export default Note
