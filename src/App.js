d0758e9dd6c4e716fb00db6b5bced90a91a5967d
charlotte-greatbatch <charlottegreatbatch01@gmail.com>
2022-08-18 11:26:57 +1200

    removed hardcoded data from note, useState to render it in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useState } from 'react'
+import { nanoid } from 'nanoid'
 import NotesList from './components/NotesList'
 
 const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: 'Test text',
+      date: '18/08/2022',
+    },
+    {
+      id: nanoid(),
+      text: 'second test',
+      date: '18/04/2022',
+    },
+    {
+      id: nanoid(),
+      text: 'third test',
+      date: '18/09/2022',
+    },
+  ])
   return (
     <div className="container">
-      <NotesList />
+      <NotesList notes={notes} />
     </div>
   )
 }
