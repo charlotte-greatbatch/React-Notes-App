fa7d1cf5623bb0238f125de66da3d9f7546de39f
charlotte-greatbatch <charlottegreatbatch01@gmail.com>
2022-08-18 11:19:14 +1200

    notes list responsive with CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import NotesList from './components/NotesList'
+
 const App = () => {
-  return <p>test</p>
+  return (
+    <div className="container">
+      <NotesList />
+    </div>
+  )
 }
 
 export default App
--- src/components/Note.js
+++ src/components/Note.js
@@ -5,7 +5,7 @@ const Note = () => {
     <div className="note">
       <span>Hello this is our first note</span>
       <div className="note-footer">
-        <small>/8/18/2022</small>
+        <small>8/18/2022</small>
         <MdDeleteForever className="delete-icon" size="1.3em" />
       </div>
     </div>
